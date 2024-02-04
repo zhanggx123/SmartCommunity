68d7b698e30b1ac0dcc38bf9467bf89c8736bfba
zhangguoxiang <Guoxiang_Z@qq.com>
2024-02-04 17:57:42 +0800

    模块调整

--- service-gateway/src/main/java/com/tqzl/gateway/config/security/SecurityConfig.java
+++ service-gateway/src/main/java/com/tqzl/gateway/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.tqzl.gateway.config;
+package com.tqzl.gateway.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
